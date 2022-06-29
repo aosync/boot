5a5d46c5981200443419c419a560217c5fb4f61e
Alejandro Sior <aho@sior.be>
2022-06-29 10:50:58 +0200

    stddef/assert.h: add static_assert

---
+++ stddef/assert.h
@@ -0,0 +1,6 @@
+#ifndef ASSERT_H
+#define ASSERT_H
+
+#define static_assert _Static_assert
+
+#endif
--- stddef/stdint.h
+++ stddef/stdint.h
@@ -1,6 +1,8 @@
 #ifndef STDINT_H
 #define STDINT_H
 
+#include <assert.h>
+
 // Normal types
 typedef unsigned char uchar;
 
@@ -16,16 +18,24 @@ typedef unsigned long long int uvlong;
 // Bit granular types available on this machine
 typedef unsigned char u8;
 typedef char i8;
+// Assumption that char is 8 bits.
 
 typedef unsigned short int u16;
 typedef short int i16;
+static_assert(sizeof(u16) == 2, "u16 is not 16-bits");
+static_assert(sizeof(i16) == 2, "i16 is not 16-bits");
 
 typedef unsigned int u32;
 typedef int i32;
+static_assert(sizeof(u32) == 4, "u32 is not 32-bits");
+static_assert(sizeof(i32) == 4, "i32 is not 32-bits");
 
 typedef unsigned long long int u64;
 typedef long long int i64;
+static_assert(sizeof(u64) == 8, "u64 is not 64-bits");
+static_assert(sizeof(i64) == 8, "i64 is not 64-bits");
 
 typedef unsigned long long int size_t;
+static_assert(sizeof(size_t) == 8, "size_t is not 64-bits");
 
 #endif

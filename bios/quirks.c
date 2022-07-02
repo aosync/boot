a639e723081ad3efdeb55c486d213da361e3e134
Alejandro Sior <aho@sior.be>
2022-07-02 10:58:41 +0200

    bios/quirks: add framers for each usable region

--- bios/quirks.c
+++ bios/quirks.c
@@ -7,6 +7,9 @@
 #include <mem/mmap.h>
 #include <str/conv.h>
 
+#define BIOS_LIMIT ((void*)0x80000000)
+#define BIOS_EBDA (*((volatile u16*)0x413) * 1024)
+
 int main();
 
 int off = 0;
@@ -14,11 +17,11 @@ int off = 0;
 // Performs the early initialization quirks 
 void bios_quirks() {
 	// Install a framer in the upper workspace
-	MemFramer *fm = mem_framer_install(&bios_upper_workspace, (void*)0x80000, 4096);
+	MemFramer *prim = mem_framer_install(&bios_upper_workspace, (void*)0x80000, 4096);
 	
 	// Allocate the initial mmap block
-	void *mmapptr = mem_framer_alloc(fm, 1);
-	MemMmapBlk *blk = mem_mmap_install(mmapptr, fm);
+	void *mmapptr = mem_framer_alloc(prim, 1);
+	MemMmapBlk *blk = mem_mmap_install(mmapptr, prim);
 
 	// Mark important defined memory regions
 	mem_mmap_mark(blk, 0, 0x400, MEM_MMAP_BIOS_IVT, 0);
@@ -26,7 +29,7 @@ void bios_quirks() {
 	mem_mmap_mark(blk, 0x500, 0x80000, MEM_MMAP_BOOTLOADER_RECLAIMABLE, 0);
 	
 	// and the EBDA
-	size_t ebda_addr = *((u16*)0x413) * 1024;
+	size_t ebda_addr = BIOS_EBDA;
 	mem_mmap_mark(blk, ebda_addr, ebda_addr + 128 * 1024, MEM_MMAP_BIOS_EBDA, 0);
 
 	// Add the E820 memory entries
@@ -43,6 +46,25 @@ void bios_quirks() {
 		mem_mmap_mark(blk, bentry->base, bentry->base + bentry->length, bentry->type, bentry->acpi_attr);
 	}
 
+	MemFramer *bulk = nil;
+	MemMmapEntry *entry;
+	for (entry = (MemMmapEntry*)blk->first; entry; entry = (MemMmapEntry*)entry->next) {
+		if (entry->type != MEM_MMAP_USABLE)
+			continue;
+		
+		void *end = (void*)entry->end;
+		if (end >= BIOS_LIMIT)
+			end = BIOS_LIMIT;
+
+		MemFramer *tmp = mem_framer_install((void*)entry->begin, end, 4096);
+		if (tmp == nil)
+			continue;
+
+		if (bulk)
+			mem_framer_append(bulk, tmp);
+		else
+			bulk = tmp;
+	}
 
 	// Debug the entries
 	short *fb = (short*)0xb8000;
@@ -50,7 +72,7 @@ void bios_quirks() {
 	for (int i = 0; i < 80*25; i++)
 		fb[i] = 0;
 
-	MemMmapEntry *entry = (MemMmapEntry*)blk->first;
+	entry = (MemMmapEntry*)blk->first;
 	while (entry) {
 
 		int i, j;
